08ed701d6131fa3727c19289ed033399de2eac33
Simon Lidberg <simon.lidberg@gmail.com>
2021-01-23 16:33:39 +0100

    Improvements to loading
    Handles files better now

--- R/load.R
+++ R/load.R
@@ -9,12 +9,16 @@
 #'
 #' @return A dataframe with the data
 loaddataset <- function(filename, objectives=NULL, inputs=NULL, outputs=NULL, custom_outputs=NULL) {
-  opt_info <- readr::read_lines(filename,skip_empty_rows = T) %>% stringr::str_replace_all("[\r\n]", "")
+  opt_info <- readr::read_lines(filename,skip_empty_rows = T) %>% stringr::str_replace_all("[\r\n]", "") %>% stringr::str_replace_all(",", ".")
+  # counts <- c(sum(stringr::str_count(opt_info, ".")), sum(stringr::str_count(opt_info, ",")))
+  # decimal_mark <- dplyr::if_else(max(counts)==counts[1],".",",")
+  # opt_info <- stringr::str_replace_all(opt_info, )
+  # cust_locale <- readr::locale(decimal_mark=dplyr::if_else(max(counts)==counts[1],".",","))
+  cust_locale <- readr::locale(decimal_mark=".")
 
   opt <- opt_info %>%
-    #paste(collapse = "\n") %>%
     utils::head(.,-4) %>%
-    readr::read_delim(delim=";",trim_ws = T) %>%
+    readr::read_delim(delim=";",trim_ws = T, locale = cust_locale) %>%
     dplyr::select(-dplyr::any_of(c("Replications", "ConstraintViolation")), maxOut = dplyr::starts_with("maxTP")) %>%
     dplyr::select(where(function(x){!all(is.na(x))}))
 
@@ -25,11 +29,11 @@ loaddataset <- function(filename, objectives=NULL, inputs=NULL, outputs=NULL, cu
     paste(collapse = "\n")
 
   opt_id <- opt_info %>%
-    readr::read_delim(delim=";", n_max = 1, col_names = FALSE,trim_ws = T) %>%
+    readr::read_delim(delim=";", n_max = 1, col_names = FALSE,trim_ws = T, locale = cust_locale) %>%
     dplyr::pull(2)
 
   opt_parameters <- opt_info %>%
-    readr::read_delim(delim=";", skip = 1, n_max = 1, col_names = FALSE,trim_ws = T) %>%
+    readr::read_delim(delim=";", skip = 1, n_max = 1, col_names = FALSE,trim_ws = T, locale = cust_locale) %>%
     dplyr::select(!!-1, -((length(.)-1):length(.))) %>%
     dplyr::slice() %>%
     unlist(., use.names = FALSE)
@@ -38,7 +42,7 @@ loaddataset <- function(filename, objectives=NULL, inputs=NULL, outputs=NULL, cu
   #  readr::read_csv2(skip = 3, n_max = 1, col_names = FALSE)
 
   opt_objectives <- opt_info %>%
-    readr::read_delim(delim=";", skip = 3, n_max = 1, col_names = FALSE,trim_ws = T) %>%
+    readr::read_delim(delim=";", skip = 3, n_max = 1, col_names = FALSE,trim_ws = T, locale = cust_locale) %>%
     dplyr::select_if(grepl("Min|Max", .)) %>%
     grepl("Max", .)
 
@@ -49,11 +53,11 @@ loaddataset <- function(filename, objectives=NULL, inputs=NULL, outputs=NULL, cu
   # TODO: Change this to enable importing of custom optimization examples
   if (is.null(inputs) || length(inputs)==0) {
     # Try general SCORE naming
-    if (grep("^imp_", opt_parameters)) {
+    if (any(grep("^imp_", opt_parameters))) {
       inputs <- grep("^imp_", opt_parameters, value = TRUE)
     } else {
       # Set inputs to everything other than outputs and objectives
-
+      inputs <- opt_parameters[!(opt_parameters %in% names(opt_objectives))]
     }
   }
 
@@ -61,10 +65,10 @@ loaddataset <- function(filename, objectives=NULL, inputs=NULL, outputs=NULL, cu
 
   opt_results <- optresult(data = opt,
                            opt_name = tools::file_path_sans_ext(basename(filename)),
-                           id = opt_id,
+                           opt_id = opt_id,
                            objectives = opt_objectives,
                            inputs = inputs,
-                           outputs = outputs)
+                           outputs = outputs,
+                           parameters = opt_parameters)
   return(opt_results)
 }
-
