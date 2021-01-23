fbbbe73615caddf5921220e244ba5331dd3c3b66
Simon Lidberg <simon.lidberg@gmail.com>
2021-01-23 16:33:05 +0100

    Added custom accessors to OptResult

--- R/SCOREr.R
+++ R/SCOREr.R
@@ -1,12 +1,42 @@
 #S3 implementation
-new_optresult <- function(data, opt_name, id, objectives, inputs, outputs, parameters) {
+new_optresult <- function(data, opt_name, opt_id, objectives, inputs, outputs, parameters) {
   tibble::validate_tibble(tibble::new_tibble(data, class = "OptResult",
                                              opt_name = opt_name,
-                                             id = id,
+                                             opt_id = opt_id,
                                              objectives = objectives,
                                              inputs = inputs,
                                              outputs = outputs,
                                              parameters = parameters))
+}
+
+#' @export
+`$.OptResult` <- function(x, value, ...) {
+  #stopifnot(is.OptResult(value))
+  if (value %in% c("objectives", "inputs", "outputs", "parameters", "opt_name", "opt_id")) {
+    if (value=="objectives"){
+      names(attr(x, value))
+    } else {
+      attr(x, value)
+    }
+  } else {
+    NextMethod(x, value, ...)
+  }
+}
+
+#' @export
+`$<-.OptResult` <- function(x, name, value) {
+  #stopifnot(is.OptResult(value))
+  #var <- unlist(...)
+  if (name %in% c("objectives", "inputs", "outputs", "parameters", "opt_name", "opt_id")) {
+    if (name =="objectives"){
+      names(attr(x, name)) <- value
+    } else {
+      attr(x, name) <- value
+    }
+    return(x)
+  } else {
+    NextMethod(x, name, value)
+  }
 }
 
 #' Title
@@ -21,29 +51,62 @@ new_optresult <- function(data, opt_name, id, objectives, inputs, outputs, param
 #' @return An S3 class
 #' @export
 #'
-optresult <- function(data, opt_name, id=1, objectives, inputs, outputs, parameters) {
-  new_optresult(data, opt_name = opt_name, id = id, objectives = objectives, inputs = inputs, outputs = outputs, parameters = parameters)
+optresult <- function(data, opt_name, opt_id=1, objectives, inputs, outputs, parameters) {
+  new_optresult(data, opt_name = opt_name, opt_id = opt_id, objectives = objectives, inputs = inputs, outputs = outputs, parameters = parameters)
 }
 
 
 #S4 implementation
-# setOldClass(c("tbl_df", "tbl", "data.frame"))
-#
-# setClass("OptResults",
+#setOldClass(c("tbl_df", "tbl", "data.frame"))
+# setOldClass("data.frame")
+# setClass(Class="OptResult",
 #          slots = c(
 #            opt_name = "character",
 #            id = "numeric",
 #            objectives = "logical",
 #            inputs = "character",
-#            outputs = "character"
+#            outputs = "character",
+#            parameters = "character"
 #          ),
-#          data = function(x) show(x),
-#          contains = "tbl_df")
-#
-# setMethod("show", signature(object="OptResults"), function(object) {
-#   as(object, "S3")
+#          #data = function(x) show(x),
+#          contains = "data.frame")#c("tbl_df", "tbl", "data.frame"))#class(tibble::tibble())
+
+# setMethod("show", signature(object="OptResult"), function(object) {
+#   object
 # })
+# setGeneric("print", function(object) object)
+# setMethod("print", signature(object="OptResult"),function(object) {
+#   object
+# })
+
+# OptResults <- function(data, opt_name, id=1, objectives, inputs, outputs, parameters) {
+#   new("OptResult", data, opt_name = opt_name, id = id, objectives = objectives, inputs = inputs, outputs = outputs, parameters = parameters)
+# }
 #
-# OptResults <- function(opt_name, id, objectives, inputs, outputs, data) {
-#   new("OptResults", data, opt_name = opt_name, id = id, objectives = objectives, inputs = inputs, outputs = outputs)
+# OptResultR6 <- R6::R6Class("OptResult",
+#   public = list(
+#     opt_name = "character",
+#     id = "numeric",
+#     objectives = "logical",
+#     inputs = "character",
+#     outputs = "character",
+#     parameters = "character",
+#     data = class(tibble::tibble()),
+#     initialize = function(data, opt_name, id=1, objectives, inputs, outputs, parameters) {
+#       self$data <- tibble::validate_tibble(tibble::new_tibble(data))
+#       self$opt_name <- opt_name
+#       self$id <- id
+#       self$objectives <- objectives
+#       self$inputs <- inputs
+#       self$outputs <- outputs
+#       self$parameters <- parameters
+#     },
+#     print = function(...) {
+#       print(self$data)
+#       #invisible(self)
+#     })
+# )
+#
+# newR6 <- function(data, opt_name, id=1, objectives, inputs, outputs, parameters) {
+#   OptResultR6$new(data, opt_name = opt_name, id = id, objectives = objectives, inputs = inputs, outputs = outputs, parameters = parameters)
 # }
