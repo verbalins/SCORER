1b12388f1e0a6da2ee495a0ff7ced87169f32264
Simon Lidberg <simon.lidberg@gmail.com>
2021-07-01 11:05:01 +0200

    Extracted code for export functionality
    Moved into app_export.R

---
+++ R/app_export.R
@@ -0,0 +1,146 @@
+# UI for the export tab
+# Handles:
+# - Download of data at different states
+# - Export rules
+# - Export code for reproducibility
+mod_export_ui <- function(id) {
+  ns <- shiny::NS(id)
+  shiny::tagList(
+    shiny::fillPage(
+      shinydashboard::box(title = "Download data",
+                          shiny::helpText("Select the data to download:"),
+                          shiny::radioButtons(ns("download_select"),
+                                              label = "",
+                                              choices = c("Imported",
+                                                          "Filtered",
+                                                          "Selected",
+                                                          "Rules",
+                                                          "Code")),
+                          shiny::downloadButton(ns("export_data"), "Download")
+      ),
+      shinydashboard::box(title = "Generated Code",
+                          shiny::htmlOutput(ns("generate_r_code")))
+    )
+  )
+}
+
+mod_export_server <- function(id, r) {
+  shiny::moduleServer(
+    id,
+    function(input, output, session) {
+      output$export_data <- shiny::downloadHandler(
+        filename = function() {
+          opt_name <- r$filtered_data$opt_name
+          paste0(opt_name, "-",
+                 input$download_select,
+                 ifelse(input$download_select == "Code", ".R", ".csv"))
+        },
+        content = function(file) {
+          if (input$download_select == "Code") {
+            get_code() %>%
+              stringr::str_replace_all(pattern = "<br>",
+                                       replacement = "\n") %>%
+              readr::write_lines(file = file)
+          } else {
+            data <- switch(input$download_select,
+                           "Imported" = r$data,
+                           "Filtered" = r$filtered_data,
+                           "Selected" = r$df_selected()$sel,
+                           "Rules" = r$rules_r)
+            write.csv2(as.data.frame(data), file, row.names = FALSE)
+          }
+        }
+      )
+
+      # Get code for the analysis
+      get_code <- shiny::reactive({
+        pastevector  <- function(x, y) {
+          paste0(x,
+                 " = c(",
+                 paste0("\"", y, "\"", collapse = ", "),
+                 ")")
+        }
+
+        createfilter <- function(x, y) {
+          val <- unlist(stringr::str_split(x, " ... "))
+          paste0("dplyr::between(",
+                 paste(y, val[1], val[2], sep = ", "),
+                 ")")
+        }
+
+        load_data <- paste0("df <- SCORER::loaddataset(file = \"",
+                            r$filepath,
+                            "\",<br>",
+                            pastevector("objectives", r$data$objectives),
+                            ",<br>",
+                            pastevector("inputs", r$data$inputs),
+                            ",<br>",
+                            pastevector("outputs", r$data$outputs),
+                            ")<br>")
+
+        import_data <- paste0("df_imported <- df %>% <br>",
+                              "dplyr::select(",
+                              paste("Iteration",
+                                    paste(r$data$objectives, collapse = ", "),
+                                    paste(r$data$inputs, collapse = ", "),
+                                    paste(r$data$outputs, collapse = ", "),
+                                    "dplyr::starts_with(c('Rank', 'Distance'))",
+                                    collapse = ", ",
+                                    sep = ", "),
+                              ")<br>")
+
+        if (is.null(r$filters)) {
+          filter_data <- paste0("df_filtered <- df_imported #No filters applied <br>")
+        } else {
+          filters <- unlist(r$filters)
+          names(filters) <- colnames(r$data)
+          filters <- filters[filters != ""]
+          filter_col <- mapply(createfilter, filters, names(filters))
+          filter_data <- paste0("df_filtered <- df_imported %>% <br>",
+                                "dplyr::filter(",
+                                paste0(filter_col,
+                                       collapse = ", "),
+                                ")<br>")
+        }
+
+        clustered_data <- paste0("df_clustered <- df_filtered # Not implemented <br>")
+
+        if (nrow(r$df_selected()$unsel) == 0) {
+          selected_data <- "df_selected <- df_clustered # No selected solutions <br> "
+        } else {
+          selected_data <- paste("df_selected <- df_clustered %>% <br>",
+                                 "dplyr::filter(Iteration %in%",
+                                 paste0("c(",
+                                        paste0(r$df_selected()$sel$Iteration,
+                                               collapse = ", "),
+                                        ")"),
+                                 ") <br>")
+        }
+
+        # Rules will be dependent on weather we have assigned a reference point
+        #  or selected iterations manually
+
+        rules <- paste("rules <- SCORER::fpm(df_clustered",
+                       paste0("max_level = ", r$fpmlevel),
+                       paste0("min_sig = ", r$minsig),
+                       "selected_data = df_selected$Iteration)",
+                       sep = ", <br>")
+
+        preamble <- paste0("library(SCORER)<br>",
+                           "set.seed(",
+                           r$randomseed,
+                           ")<br>")
+        paste(preamble,
+              load_data,
+              import_data,
+              filter_data,
+              clustered_data,
+              selected_data,
+              rules, sep = "<br>")
+      })
+
+      output$generate_r_code <- shiny::renderText({
+        get_code()
+      })
+    }
+)}
--- browser/app.R
+++ browser/app.R
@@ -40,26 +40,11 @@
 seed <- 42
 set.seed(seed)
 options(shiny.maxRequestSize = 30 * 1024 ^ 2) # 30MB
-options(shiny.error = browser)
-#options(shiny.reactlog = TRUE)
+#options(shiny.error = browser)
+options(shiny.reactlog = TRUE)
+options(display.mode = "showcase")
 
 debug <- FALSE
-
-export_ui <- shiny::fillPage(
-    shinydashboard::box(title = "Download data",
-        shiny::helpText("Specify the data to download."),
-        shiny::radioButtons("downloadSelect",
-                            label = "",
-                            choices = c("Imported",
-                                        "Filtered",
-                                        "Selected",
-                                        "Rules",
-                                        "Code")),
-        shiny::downloadButton("export_data", "Download")
-    ),
-    shinydashboard::box(title = "Generated Code",
-                        shiny::htmlOutput("generate_r_code"))
-)
 
 # Sidebar UI
 sidebar <- shinydashboard::dashboardSidebar(
@@ -99,7 +84,7 @@ body <- shinydashboard::dashboardBody(
         shinydashboard::tabItem(tabName = "rule",
                                 mod_rule_ui("rule")),
         shinydashboard::tabItem(tabName = "export",
-                                export_ui)
+                                mod_export_ui("export"))
     )
 )
 
@@ -114,7 +99,9 @@ server <- function(input, output, session) {
     # reactiveValues used to hold the data in the app,
     #  communication between modules.
     # TODO: Default values set, change for prod
-    r <- shiny::reactiveValues(data = SCORER::FMC, filtered_data = SCORER::FMC)
+    r <- shiny::reactiveValues(data = SCORER::FMC,
+                               filtered_data = SCORER::FMC,
+                               randomseed = seed)
 
     ### Data import logic --------------------------------------------
     mod_import_server("import", r)
@@ -138,116 +125,7 @@ server <- function(input, output, session) {
     mod_rule_server("rule", r)
 
     ### Data export logic --------------------------------------------
-    output$export_data <- shiny::downloadHandler(
-        filename = function() {
-            opt_name <- df_filtered()$data$opt_name
-            paste0(opt_name, "-",
-                   input$downloadSelect,
-                   ifelse(input$downloadSelect == "Code", ".R", ".csv"))
-        },
-        content = function(file) {
-            if (input$downloadSelect == "Code") {
-                get_code() %>%
-                    stringr::str_replace_all(pattern = "<br>",
-                                             replacement = "\n") %>%
-                    readr::write_lines(file = file)
-            } else {
-                data <- switch(input$downloadSelect,
-                               "Imported" = r$data,
-                               "Filtered" = r$filtered_data,
-                               "Selected" = r$df_selected(),
-                               "Rules" = r$R)
-                write.csv2(as.data.frame(data), file, row.names = FALSE)
-            }
-        }
-    )
-
-    # Get code for the analysis
-    get_code <- shiny::reactive({
-        pastevector  <- function(x, y) {
-          paste0(x,
-                 " = c(",
-                 paste0("\"", y, "\"", collapse = ", "),
-                 ")")
-        }
-
-        createfilter <- function(x, y) {
-            val <- unlist(stringr::str_split(x, " ... "))
-            paste0("dplyr::between(",
-                   paste(y, val[1], val[2], sep = ", "),
-                   ")")
-        }
-
-        load_data <- paste0("df <- SCORER::loaddataset(file=\"",
-                            r$filepath,
-                            "\",<br>",
-                            pastevector("objectives", r$data$objectives),
-                            ",<br>",
-                            pastevector("inputs", r$data$inputs),
-                            ",<br>",
-                            pastevector("outputs", r$data$outputs),
-                            ")<br>")
-
-        import_data <- paste0("df_imported <- df %>% <br>",
-                              "dplyr::select(",
-                              paste("Iteration",
-                                paste(r$data$objectives, collapse = ", "),
-                                paste(r$data$inputs, collapse = ", "),
-                                paste(r$data$outputs, collapse = ", "),
-                                "dplyr::starts_with(c('Rank', 'Distance'))",
-                                collapse = ", ",
-                                sep = ", "),
-                              ")<br>")
-
-        filters <- unlist(r$filters)
-        names(filters) <- colnames(r$data)
-        filters <- filters[filters != ""]
-        filter_col <- mapply(createfilter, filters, names(filters))
-        if (length(filter_col) == 0) {
-            filter_data <- paste0("df_filtered <- df_imported #No filters applied <br>")
-        } else {
-            filter_data <- paste0("df_filtered <- df_imported %>% <br>",
-                                  "dplyr::filter(",
-                                  paste0(filter_col,
-                                         collapse = ", "),
-                                  ")<br>")
-        }
-
-        clustered_data <- paste0("df_clustered <- df_filtered # Not implemented <br>")
-
-        if (nrow(r$df_selected()$unsel) == 0) {
-            selected_data <- "df_selected <- df_clustered # No selected solutions <br> "
-        } else {
-            selected_data <- paste("df_selected <- df_clustered %>% <br>",
-                                   "dplyr::filter(Iteration %in%",
-                                   paste0("c(",
-                                          paste0(r$df_selected()$sel$Iteration,
-                                                 collapse = ", "),
-                                          ")"),
-                                   ") <br>")
-        }
-
-        rules <- paste("rules <- SCORER::fpm(df_clustered",
-                       paste0("max_level = ", r$fpmlevel),
-                       paste0("min_sig = ", r$dataminsig),
-                       "selected_data = df_selected$Iteration)", sep = ", <br>")
-
-        preamble <- paste0("library(SCORER)<br>",
-                           "set.seed(",
-                           seed,
-                           ")<br>")
-        paste(preamble,
-              load_data,
-              import_data,
-              filter_data,
-              clustered_data,
-              selected_data,
-              rules, sep = "<br>")
-    })
-
-    output$generate_r_code <- shiny::renderText({
-        get_code()
-    })
+    mod_export_server("export", r)
 }
 
 # Run the application
